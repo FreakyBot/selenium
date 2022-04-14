4dc2e194bf1c72bbfe643f471ebb0aa27c81a6d7
kkaletka <kamil.kaletka@salesmanago.com>
2022-04-14 17:22:53 +0200

    test commit

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-import time
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import *
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-s = 'K://pythonProject/venv/chromedriver-Windows'
-driver = webdriver.Chrome(s)
+s = Service('/usr/bin/chromedriver')
+driver = webdriver.Chrome(service=s)
 
-# path = 'K://pythonProject/venv/chromedriver-Windows'
-# driver = webdriver.Chrome(path)
+driver.get("https://www.salesmanago.com/info/knowledgecenter.htm")
 
-# navigating to
-driver.get('https://www.salesmanago.pl/info/knowledgecenter.htm')
+wait = WebDriverWait(driver, 10)
 
-elems = driver.find_elements(By.XPATH, "//a[@href]")
+element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="close-cookies"]'))).click()
+# element1 = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'VizExNotificationBadge__Wrapper-b3snvh-0 kanpaf'))).click()
+driver.switchTo().frame(driver.find_element(By.XPATH, '//*[@id="hubspot-messages-iframe-container'))
+driver.find_element(By.CLASS_NAME("VizExNotificationBadge__Wrapper-b3snvh-0 kanpaf")).click()
+driver.switchTo().defaultContent()
+
+elems = driver.find_elements(By.CSS_SELECTOR, "div.ebook__img--container a")
 
 
-for elem in elems:
-    print(elem.get_attribute('href'))
+driver.get(elems[4].get_attribute('href'))
 
-# elems = driver.find_elements(By.CSS_SELECTOR, ".ebook__img--container [href]")
-# links = {elem.get_attribute('href') for elem in elems}
+driver.find_element(By.NAME, 'name').send_keys('test')
+driver.find_element(By.NAME, 'email').send_keys('test@o2.pl')
+driver.find_element(By.NAME, 'company').send_keys('test')
+driver.find_element(By.NAME, 'url').send_keys('https://test.pl')
+driver.find_element(By.NAME, 'phoneNumber').send_keys('666666666')
 
-# printing length of total i.e total number of links
-# print(len(total))
-# print("Following are the link present in the webpage")
-# for totals in total:
-#     total.get_attribute("href")
-#     print(totals.text)
 
-driver.close()
+
+
+# driver.close()
